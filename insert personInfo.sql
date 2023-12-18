4c42d5e601007eb52d44a0b0d64f50c6cdca1462
Talukder Abdullah Al Talha <71594697+Talha260599@users.noreply.github.com>
2023-12-19 02:22:26 +0600

    insert person information

---
+++ insert personInfo.sql
@@ -0,0 +1,5 @@
+INSERT INTO Person (Name, Age, timestamp) VALUES
+('Rohit', 29, NOW()),
+('Sumon', 31, NOW() - INTERVAL 10 SECOND),
+('Bijoy', 26, NOW() - INTERVAL 20 SECOND),
+('Fatema', 28, NOW() - INTERVAL 30 SECOND);
